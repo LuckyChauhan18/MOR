3103b6442855331d0271b292cc683a7e5e44a866
Lucky Chauhan <luckymilak243@gmail.com>
2026-02-20 14:53:00 +0530

    Fix backend crash: add missing path import

--- blog_web_app/backend/server.js
+++ blog_web_app/backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 dotenv.config();
 
